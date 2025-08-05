a0a1a1f00313908e02e5e3dc9c9d7e80385c5820
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-08-05 22:13:20 +0900

    feat: 앱 사용량 & 유저 프로필 관련 어드민 페이지 제작

--- apps/usage/admin.py
+++ apps/usage/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import AppInfo, UsageRecord
 
-# Register your models here.
+@admin.register(AppInfo)
+class AppInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'package_name', 'app_name']
+    search_fields = ['package_name', 'app_name']
+    ordering = ['app_name']
+    
+@admin.register(UsageRecord)
+class UsageRecordAdmin(admin.ModelAdmin):   
+    list_display = ['id', 'user', 'app', 'usage_time_ms', 'start_time', 'end_time', 'created_at']
+    search_fields = ['user__username', 'app__app_name']
+    list_filter = ['user', 'app']
+    autocomplete_fields = ['user', 'app']
+    ordering = ['-created_at']
+    
+    def has_add_permission(self, request):
+        return False
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'profile_image']
+    search_fields = ['user__username']
+    ordering = ['user__username']
+    
+    def has_add_permission(self, request):
+        return False
